42f7a8768410e0c47a0970c277af98d9a8593710
rex-gentium <budowniczy.zamyslow@gmail.com>
2020-04-24 02:46:58 +0500

    Added PlayerMoveEvent listener to print player coordinates

--- src/main/java/com/dicksuckers/meinkraftplugin/listeners/MyListener.java
+++ src/main/java/com/dicksuckers/meinkraftplugin/listeners/MyListener.java
@@ -1,9 +1,11 @@
 package com.dicksuckers.meinkraftplugin.listeners;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
 
 public class MyListener implements Listener {
     @EventHandler
@@ -12,4 +14,28 @@ public class MyListener implements Listener {
         Bukkit.broadcastMessage("Attention: new dicksucker arrived to the server!");
         event.setJoinMessage("Welcome to the dicksuckers club, " + event.getPlayer().getName() + "!");
     }
+
+    @EventHandler
+    public void onPlayerMove(PlayerMoveEvent event) {
+        if (event.isCancelled()) {
+            return; // movement cancelled
+        }
+        Location from = event.getFrom();
+        Location to = event.getTo();
+        if (to == null || from.equals(to)) {
+            return; // coordinated did not change
+        }
+        String playerName = event.getPlayer().getName();
+        String message = String.format("Player %s moved from %s to %s",
+                playerName, locationToString(from), locationToString(to));
+        Bukkit.broadcastMessage(message);
+    }
+
+    private String locationToString(Location location) {
+        return String.format("(%f, %f, %f)",
+                location.getX(),
+                location.getY(),
+                location.getZ());
+    }
+
 }
